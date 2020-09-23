d1a3df5dfbbb3e361d918e9d1584d3724c7284b4
zizi0302 <50587019+zizi0302@users.noreply.github.com>
2020-09-23 12:16:39 +0900

    에러 로그 남기기 체험

--- app/src/main/java/kr/co/kotlinbasic_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/kotlinbasic_20200823/MainActivity.kt
@@ -2,10 +2,17 @@ package kr.co.kotlinbasic_20200823
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        makeLogBtn.setOnClickListener {
+            Log.d("메인화면", "로그 남기기 버튼 눌림")
+            Log.d("메인화면", "실행되면 안되는 코드에 대한 체크")
+        }
     }
 }
