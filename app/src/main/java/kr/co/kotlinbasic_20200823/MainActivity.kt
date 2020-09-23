9932326d8a38b45b36b7b1e7a7f94c355af32eda
zizi0302 <50587019+zizi0302@users.noreply.github.com>
2020-09-23 12:45:32 +0900

    입력된 문구를 받아서 토스트로 출력

--- app/src/main/java/kr/co/kotlinbasic_20200823/MainActivity.kt
+++ app/src/main/java/kr/co/kotlinbasic_20200823/MainActivity.kt
@@ -25,5 +25,15 @@ class MainActivity : AppCompatActivity() {
 //            사용자에게 "버튼 눌림" 안내 문구 제공
             Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
         }
+
+//        입력한 문구를 받아서 => 토스트로 출력
+        contentToastBtn.setOnClickListener {
+//            contentEdt의 문구 (text 속성의 값) => (String으로) 받아서 코틀린 변수에 저장
+            val inputContent = contentEdt.text.toString()
+
+//        변수에 저장된 문구를 => 토스트의 재료로 사용
+            Toast.makeText(this, inputContent, Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
